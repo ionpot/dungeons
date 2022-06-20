5d8159ef664b706fe7fd3fab0dabaff11d99f179
Emir Kotan <emirkotan@gmail.com>
2022-06-20 20:48:04 +0100

    roll an enemy orc in combat screen

--- dungeons/combat_screen.cpp
+++ dungeons/combat_screen.cpp
@@ -6,6 +6,8 @@
 #include <ui/context.hpp>
 #include <ui/entity_info.hpp>
 
+#include <game/context.hpp>
+#include <game/entity.hpp>
 #include <game/log.hpp>
 #include <game/string.hpp>
 
@@ -20,17 +22,22 @@ namespace dungeons {
 	CombatScreen::CombatScreen(
 			std::shared_ptr<game::Log> log,
 			const ui::Context& ui,
+			game::Context& game,
 			const screen::ToCombat& input
 	):
 		m_log {log},
+		m_enemy {std::make_shared<game::Entity>(game.roll_orc())},
 		m_player_info {std::make_shared<ui::EntityInfo>(ui, *input.player)},
+		m_enemy_info {std::make_shared<ui::EntityInfo>(ui, *m_enemy)},
 		m_button {std::make_shared<ui::Button>(ui, "Done")}
 	{
 		elements({m_button});
-		groups({m_player_info});
+		groups({m_player_info, m_enemy_info});
 		m_player_info->position(ui.screen_margin);
+		m_enemy_info->place_after(*m_player_info, ui.section_spacing);
 		m_button->place_below(*m_player_info, ui.section_spacing);
-		m_log->put("Combat begins.");
+		m_log->put("Enemy");
+		m_log->entity(*m_enemy);
 	}
 
 	std::optional<screen::Output>
--- dungeons/combat_screen.hpp
+++ dungeons/combat_screen.hpp
@@ -6,6 +6,8 @@
 #include <ui/context.hpp>
 #include <ui/entity_info.hpp>
 
+#include <game/context.hpp>
+#include <game/entity.hpp>
 #include <game/log.hpp>
 
 #include <ionpot/widget/element.hpp>
@@ -22,13 +24,16 @@ namespace dungeons {
 		CombatScreen(
 			std::shared_ptr<game::Log>,
 			const ui::Context&,
+			game::Context&,
 			const screen::ToCombat&);
 
 		std::optional<screen::Output> on_click(const widget::Element&);
 
 	private:
 		std::shared_ptr<game::Log> m_log;
+		std::shared_ptr<game::Entity> m_enemy;
 		std::shared_ptr<ui::EntityInfo> m_player_info;
+		std::shared_ptr<ui::EntityInfo> m_enemy_info;
 		std::shared_ptr<ui::Button> m_button;
 	};
 }
