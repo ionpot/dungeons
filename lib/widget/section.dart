b46ace14cd2faab0576d98b8e296bda3a7007364
Emir Kotan <emirkotan@gmail.com>
2022-09-26 00:36:28 +0100

    section widget

---
+++ lib/widget/section.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/widgets.dart';
+
+class Section extends StatelessWidget {
+  final double left;
+  final double top;
+  final Widget? child;
+
+  const Section({super.key, this.child, this.left = 40, this.top = 20});
+  const Section.after({key, child}) : this(key: key, child: child, top: 0);
+  const Section.below({key, child}) : this(key: key, child: child, left: 0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: left, top: top),
+      child: child,
+    );
+  }
+}
