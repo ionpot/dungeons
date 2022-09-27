14345ce51b70967d9fba942b5a2ae55ad03f5b82
Emir Kotan <emirkotan@gmail.com>
2022-09-27 22:45:21 +0100

    ClickDent widget

--- lib/widget/button.dart
+++ lib/widget/button.dart
@@ -1,7 +1,8 @@
 import 'package:dungeons/widget/button_child.dart';
+import 'package:dungeons/widget/click_dent.dart';
 import 'package:flutter/widgets.dart';
 
-class Button extends StatefulWidget {
+class Button extends StatelessWidget {
   final String text;
   final VoidCallback onClick;
   final bool clickable;
@@ -14,31 +15,12 @@ class Button extends StatefulWidget {
   });
 
   @override
-  State<Button> createState() => _ButtonState();
-}
-
-class _ButtonState extends State<Button> {
-  bool _heldDown = false;
-
-  _ButtonState();
-
-  double _checkMargin(double margin) {
-    if (widget.clickable && _heldDown) {
-      return margin;
-    }
-    return 0;
-  }
-
-  @override
-  Widget build(BuildContext _) {
-    return GestureDetector(
-      onTapDown: (_) => setState(() => _heldDown = true),
-      onTapUp: (_) => setState(() => _heldDown = false),
-      onTapCancel: () => setState(() => _heldDown = false),
-      onTap: () => {if (widget.clickable) widget.onClick()},
-      child: Container(
-        margin: EdgeInsets.only(left: _checkMargin(2), top: _checkMargin(4)),
-        child: ButtonChild(text: widget.text),
+  Widget build(BuildContext context) {
+    return ClickDent(
+      enabled: clickable,
+      child: GestureDetector(
+        onTap: () => {if (clickable) onClick()},
+        child: ButtonChild(text: text),
       ),
     );
   }
