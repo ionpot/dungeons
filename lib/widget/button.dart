9eb275f726ff7f8552bbac2d80b0344e5714c3c7
Emir Kotan <emirkotan@gmail.com>
2022-09-30 22:10:38 +0100

    ButtonActive widget -> Button(active:)

--- lib/widget/button.dart
+++ lib/widget/button.dart
@@ -1,17 +1,20 @@
 import 'package:dungeons/widget/button_child.dart';
 import 'package:dungeons/widget/click_dent.dart';
+import 'package:dungeons/widget/colors.dart';
 import 'package:flutter/widgets.dart';
 
 class Button extends StatelessWidget {
   final String text;
   final VoidCallback onClick;
   final bool clickable;
+  final bool active;
 
   const Button({
     super.key,
     required this.text,
     required this.onClick,
     this.clickable = true,
+    this.active = false,
   });
 
   @override
@@ -20,7 +23,10 @@ class Button extends StatelessWidget {
       enabled: clickable,
       child: GestureDetector(
         onTap: () => {if (clickable) onClick()},
-        child: ButtonChild(text: text),
+        child: ButtonChild(
+          text: text,
+          color: active ? yellow : white,
+        ),
       ),
     );
   }
