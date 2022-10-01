2c59d3ba59e08d685c468932e6da895f7a316506
Emir Kotan <emirkotan@gmail.com>
2022-10-01 23:34:00 +0100

    radio buttons

---
+++ lib/widget/radio_group.dart
@@ -0,0 +1,40 @@
+import 'package:dungeons/widget/radio_button.dart';
+import 'package:dungeons/widget/spaced_row.dart';
+import 'package:flutter/widgets.dart';
+
+class RadioGroup<T extends RadioValue> extends StatefulWidget {
+  final List<T> values;
+  final ValueChanged<T> onChange;
+
+  const RadioGroup({super.key, required this.values, required this.onChange});
+
+  @override
+  State<RadioGroup<T>> createState() => _RadioGroupState();
+}
+
+class _RadioGroupState<T extends RadioValue> extends State<RadioGroup<T>> {
+  T? chosen;
+
+  @override
+  Widget build(BuildContext context) {
+    return buildSpacedRow(
+      spacing: 10,
+      children: List<Widget>.from(
+        widget.values.map(
+          (e) => _buildButton(e),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildButton(T value) {
+    return RadioButton<T>(
+      value: value,
+      chosen: value == chosen,
+      onChosen: (newValue) {
+        setState(() => chosen = newValue);
+        widget.onChange(newValue);
+      },
+    );
+  }
+}
