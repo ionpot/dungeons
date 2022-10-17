3a799202ad82641e0d78ce728b424b352a08fd8f
Emir Kotan <emirkotan@gmail.com>
2022-10-17 19:35:28 +0100

    amend window size/title for windows

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -26,8 +26,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280, 720);
-  if (!window.CreateAndShow(L"dungeons", origin, size)) {
+  Win32Window::Size size(1024, 768);
+  if (!window.CreateAndShow(L"Dungeons", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
