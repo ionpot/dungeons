b7d3c6f82d8e9b1dfc32f07861707defe88f65db
Emir Kotan <emirkotan@gmail.com>
2022-11-05 22:46:24 +0000

    EffectBonus operator +

--- lib/game/effect_bonus.dart
+++ lib/game/effect_bonus.dart
@@ -12,4 +12,25 @@ class EffectBonus {
     this.dodgeScale,
     this.hitChance,
   });
+
+  EffectBonus operator +(EffectBonus other) {
+    return EffectBonus(
+      damage: _addInt(damage, other.damage),
+      initiative: _addInt(initiative, other.initiative),
+      dodgeScale: _addPercent(dodgeScale, other.dodgeScale),
+      hitChance: _addPercent(hitChance, other.hitChance),
+    );
+  }
+
+  int? _addInt(int? a, int? b) {
+    if (a == null) return b;
+    if (b == null) return a;
+    return a + b;
+  }
+
+  Percent? _addPercent(Percent? a, Percent? b) {
+    if (a == null) return b;
+    if (b == null) return a;
+    return a + b;
+  }
 }
