69542324e460fcec1e9208743077867caed86d2b
Emir Kotan <emirkotan@gmail.com>
2023-03-11 21:47:31 +0000

    display version in Windows title

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1024, 768);
-  if (!window.CreateAndShow(L"Dungeons", origin, size)) {
+  if (!window.CreateAndShow(L"Dungeons v" FLUTTER_VERSION, origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
