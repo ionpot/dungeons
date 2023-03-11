3f12f80993ad213fd1e63b05a5e600528d5054e7
Emir Kotan <emirkotan@gmail.com>
2023-03-11 18:16:21 +0000

    pregen combat

--- lib/widget/screens.dart
+++ lib/widget/screens.dart
@@ -1,8 +1,12 @@
+import 'package:dungeons/game/armor.dart';
 import 'package:dungeons/game/combat.dart';
 import 'package:dungeons/game/entity.dart';
+import 'package:dungeons/game/entity_class.dart';
+import 'package:dungeons/game/entity_race.dart';
+import 'package:dungeons/game/gear.dart';
 import 'package:dungeons/game/log.dart';
+import 'package:dungeons/game/weapon.dart';
 import 'package:dungeons/utility/value_callback.dart';
-import 'package:dungeons/widget/character_screen.dart';
 import 'package:dungeons/widget/combat_screen.dart';
 import 'package:flutter/widgets.dart';
 
@@ -12,13 +16,15 @@ class Screens {
 
   const Screens({required this.log, required this.onNext});
 
-  Widget get first => _characterScreen();
-
-  Widget _characterScreen() {
-    return CharacterScreen(
-      onDone: (player) => onNext(_combatScreen(player)),
-    );
+  Entity get pregen {
+    return Entity(name: 'Player', race: EntityRace.human, player: true)
+      ..base.roll()
+      ..klass = EntityClass.cleric
+      ..levelUpTo(4)
+      ..equip(Gear(mainHand: Weapon.longsword, body: Armor.leather));
   }
+
+  Widget get first => _combatScreen(pregen);
 
   Widget _combatScreen(Entity player) {
     return CombatScreen(
@@ -26,7 +32,7 @@ class Screens {
       key: UniqueKey(),
       log: log,
       onWin: () => onNext(_combatScreen(player)),
-      onLose: () => onNext(_characterScreen()),
+      onLose: () => onNext(first),
     );
   }
 }
